ee982865346d9c19fd45aae78543a45937878ada
lets <wilgoncalves98@gmail.com>
2024-10-12 15:43:19 -0300

    resolução desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿using DesafioPOO.Models;
 
+Nokia n = new Nokia("1234", "Nokia", "111111", 196);
+Iphone i = new Iphone("12366", "Iphoone", "221111", 196);
+
+n.InstalarAplicativo("Instagram");
+i.InstalarAplicativo("Whatsapp");
 // TODO: Realizar os testes com as classes Nokia e Iphone
